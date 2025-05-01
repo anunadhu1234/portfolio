6d733926ef90d36f0d6bd0112eb1b9afcaebd53e
Anurag ghosh <ag952006@student.nitw.ac.in>
2025-05-01 12:46:47 +0530

    bug fix

--- src/Components/ui/animated-testimonials.tsx
+++ src/Components/ui/animated-testimonials.tsx
@@ -118,31 +118,7 @@ export const AnimatedTestimonials = ({
             <p className="text-sm text-[#7551fb] dark:text-neutral-500">
               {testimonials[active].designation}
             </p>
-            <motion.p className="mt-8 text-lg text-gray-500 dark:text-neutral-300">
-              {testimonials[active].quote.split(" ").map((word, index) => (
-                <motion.span
-                  key={index}
-                  initial={{
-                    filter: "blur(10px)",
-                    opacity: 0,
-                    y: 5,
-                  }}
-                  animate={{
-                    filter: "blur(0px)",
-                    opacity: 1,
-                    y: 0,
-                  }}
-                  transition={{
-                    duration: 0.2,
-                    ease: "easeInOut",
-                    delay: 0.02 * index,
-                  }}
-                  className="inline-block"
-                >
-                  {word}&nbsp;
-                </motion.span>
-              ))}
-            </motion.p>
+           
           </motion.div>
           <div className="flex gap-4 pt-12 md:pt-0">
             <button
