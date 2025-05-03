46af281924b3c65c4ea1f0c9a6ce3c7a3dba9371
Anurag ghosh <ag952006@student.nitw.ac.in>
2025-05-03 13:58:50 +0530

    some color changes

--- src/Components/FloatingContact/Floating Contact.tsx
+++ src/Components/FloatingContact/Floating Contact.tsx
@@ -1,8 +1,20 @@
-import { useState } from "react"
+import { useState,  useEffect } from "react"
 import { Mail, Phone, MessageCircle, Plus, X } from "lucide-react";
 
 function FloatingContact (){
     const [open, setOpen] = useState(false);
+    const [scrolled,setScrolled] = useState(false);
+    useEffect(()=>{
+        const handleScroll = () =>{
+          if(window.scrollY>200){
+            setScrolled(true);
+          }else{
+            setScrolled(false);
+          }
+        }
+        window.addEventListener("scroll",handleScroll);
+        return ()=> window.removeEventListener("scroll",handleScroll);
+      },[])
   const toggleOpen = () => setOpen(!open);
     return (
         <div className="fixed bottom-6 right-6 z-50 flex flex-col items-end gap-3">
@@ -13,14 +25,14 @@ function FloatingContact (){
             target="_blank"
             rel="noopener noreferrer"
             
-            className="flex items-center gap-2 px-4 py-2 rounded-full shadow-md bg-[#7551fb] hover:bg-red-100 transition"
+            className={`flex items-center gap-2 px-4 py-2 rounded-full shadow-md ${scrolled?'bg-[#7551fb]':'bg-black'} hover:bg-red-500 transition`}
           >
             <Mail className="text-red-300" />
             <span className="text-sm font-medium text-white">Gmail</span>
           </a>
           <a
             href="tel:+918175808205"
-            className="flex items-center gap-2 px-4 py-2 rounded-full shadow-md bg-[#7551fb] hover:bg-green-100 transition"
+            className={`flex items-center gap-2 px-4 py-2 rounded-full shadow-md ${scrolled?'bg-[#7551fb]':'bg-black'} hover:bg-green-900 transition`}
           >
             <Phone className="text-green-400" />
             <span className="text-sm font-medium text-white">Call</span>
@@ -29,7 +41,7 @@ function FloatingContact (){
             href="https://wa.me/8175808205"
             target="_blank"
             rel="noopener noreferrer"
-            className="flex items-center gap-2 px-4 py-2 rounded-full shadow-md bg-[#7551fb] hover:bg-green-200 transition"
+            className={`flex items-center gap-2 px-4 py-2 rounded-full shadow-md ${scrolled?'bg-[#7551fb]':'bg-black'} hover:bg-green-600 transition`}
           >
             <MessageCircle className="text-green-400" />
             <span className="text-sm font-medium text-white">WhatsApp</span>
@@ -39,7 +51,7 @@ function FloatingContact (){
 
       <button
         onClick={toggleOpen}
-        className="w-14 h-14 rounded-full bg-blue-600 hover:bg-blue-700 text-white flex items-center justify-center shadow-lg transition"
+        className={`w-14 h-14 rounded-full ${scrolled?'bg-blue-600':'bg-blue-800'} hover:bg-blue-700 text-white flex items-center justify-center shadow-lg transition`}
       >
         {open ? <X size={28} /> : <Plus size={28} />}
       </button>
